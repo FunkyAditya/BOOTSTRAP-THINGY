eee6c0855b0065d155757ee9c1f5cbb43ed76a2e
Aditya Prakash <114856260+FunkyAditya@users.noreply.github.com>
2023-10-05 22:09:25 +0530

    Add files via upload

---
+++ Library Website NEW/index.js
@@ -0,0 +1,20 @@
+function learn(){
+    window.location.replace("about2.html")
+}
+function replace() {
+    window.open("")
+}
+
+function book() {
+    window.open("book.html")
+}
+
+function ebook() {
+    window.open("ebook.html")
+}
+
+function magzine() {
+    window.open("magzine.html")
+}
+
+
